a3b3775e4205ee9c2948b75744ccafdf070b2d08
Jeffguardia <jeffguardia12@gmail.com>
2016-09-20 11:56:39 -0300

    Gradle 24 e método Logout implementado
    O Android Studio foi atualizado para a versão 2.2, atualizar a IDE para
    não ter problemas de  incompatibilidade.

--- FoundYou/app/src/main/java/com/example/strawberry/foundyou/ActivityBase.java
+++ FoundYou/app/src/main/java/com/example/strawberry/foundyou/ActivityBase.java
@@ -183,6 +183,7 @@ public class ActivityBase extends AppCompatActivity {
 
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(ActivityBase.this,ActivityLogin.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
                 finish();
 
