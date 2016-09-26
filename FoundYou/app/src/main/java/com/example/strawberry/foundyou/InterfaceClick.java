8df9369da9fafb2d38e4f3a31a65c0ad79fb7b5d
Leonardo Menezes <leonardomenezes.ssz@outlook.com>
2016-09-26 18:11:45 -0300

    Interface Click
    Interface de clique para listas e cardview.

---
+++ FoundYou/app/src/main/java/com/example/strawberry/foundyou/InterfaceClick.java
@@ -0,0 +1,11 @@
+package com.example.strawberry.foundyou;
+
+import android.view.View;
+
+/**
+ * Created by Leonardo on 26/09/2016.
+ */
+
+public interface InterfaceClick {
+    void onClick(View view, int postion, boolean isLongClick);
+}
